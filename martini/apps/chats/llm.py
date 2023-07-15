e6efa617f24a2b485ad2c45fd050013f35fcc54d
Davy Peter Braun <davy.braun@ginetta.net>
2023-07-15 21:40:49 +0200

    WIP

--- martini/apps/chats/llm.py
+++ martini/apps/chats/llm.py
@@ -1,6 +1,5 @@
 import os
-
-from django.conf import settings
+import openai
 
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
@@ -8,7 +7,21 @@ from langchain.embeddings import OpenAIEmbeddings
 
 from apps.documents.vectorstore import get_vectorstore_for_chains
 
-_llm = OpenAI(temperature=0, openai_api_key=os.environ.get('OPENAI_API_KEY'))
+qa_prompts = [
+    {
+        'role': 'system',
+        'content': "Use the following information to answer the question. Do not use any other information. If you don't know the answer, just say that you don't know, don't try to make up an answer"
+    },
+]
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+llm_openai = openai.ChatCompletion.create(
+    model=os.environ.get('OPENAI_MODEL'),
+    messages=qa_prompts,
+    temperature=0
+)
+
+_llm = llm_openai
 
 # In OpenAI's embedding model, <|endoftext|> is a special token that indicates the end of a document in OpenAI's embeddings.
 # We need to set this as `allowed_special` here for tiktoken to parse it without error.
